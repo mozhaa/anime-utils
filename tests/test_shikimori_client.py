142a708d1a2b174dc052223334e373e535ee3c72
mozhaa <mozhay2005@gmail.com>
2026-01-04 14:01:20 +0300

    test: add tests for shikimori client

---
+++ tests/test_shikimori_client.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+import pytest
+
+from anime_utils.clients.shikimori import ShikimoriClient
+
+
+@pytest.mark.parametrize(
+    "mal_id, info_subset",
+    [
+        (54722, {"name": "Mahou Shoujo ni Akogarete", "id": 54722, "kind": "tv"}),
+        (1453, {"name": "Maison Ikkoku", "id": 1453, "kind": "tv"}),
+        (10396, {"name": "Ben-To", "id": 10396, "kind": "tv"}),
+    ],
+)
+@pytest.mark.asyncio
+async def test_get_anime(mal_id: int, info_subset: dict[str, Any]):
+    async with ShikimoriClient() as shikimori:
+        anime = await shikimori.get_anime(mal_id)
+
+    assert anime is not None
+    for key, value in info_subset.items():
+        assert key in anime
+        assert anime[key] == value
+
+
+@pytest.mark.parametrize(
+    "query, expected_name",
+    [
+        ("Mahou Shoujo ni Akogarete", "Mahou Shoujo ni Akogarete"),
+        ("Maison Ikkoku", "Maison Ikkoku"),
+        ("Ben-To", "Ben-To"),
+    ],
+)
+@pytest.mark.asyncio
+async def test_search(query: str, expected_name: str):
+    async with ShikimoriClient() as shikimori:
+        results = await shikimori.search(query, limit=1)
+
+    assert len(results) > 0
+    assert results[0]["name"] == expected_name
